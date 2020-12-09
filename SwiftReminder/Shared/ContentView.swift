9fbd02545751beac502f99fc2ee74b6911e45277
Teppei Akada <teppei.akada@sidefeed.com>
2020-12-09 16:38:39 +0900

    first commit

---
+++ SwiftReminder/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by AKADA TEPPEI on 2020/12/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftReminder/Shared/SwiftReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftReminderApp.swift
+//  Shared
+//
+//  Created by AKADA TEPPEI on 2020/12/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
