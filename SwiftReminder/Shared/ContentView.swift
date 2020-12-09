4573f11fd248cacf89a81a6dfb4422b62448ea88
Teppei Akada <teppei.akada@sidefeed.com>
2020-12-09 18:07:29 +0900

    ボタンのレイアウト

--- SwiftReminder/Shared/ContentView.swift
+++ SwiftReminder/Shared/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {print("button pressed")}) {
+            VStack(alignment: .leading) {
+                HStack(alignment: .center) {
+                    Image(systemName: "calendar.circle.fill")
+                        .resizable()
+                        .frame(width: 30.0, height: 30.0)
+                    Spacer()
+                    Text("0")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                }
+                Text("Today")
+                    .foregroundColor(Color.gray)
+            }
+
+        }.frame(width: 100, height: 60)
     }
 }
 
